89debd22e545bd3f18a0722c35946b296fce598d
shivamPUNDIR <47301925+shivamPUNDIR@users.noreply.github.com>
2020-11-13 18:25:41 +0530

    initial
    initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# TOPSIS-ShivamPundir-101803158
+
+TOPSIS-ShivamPundir-101803158 is a Python library for calculationg TOPSIS score.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install TOPSIS-ShivamPundir-101803158
+```
+
+## Usage
+
+```python
+import TOPSIS-ShivamPundir-101803158 as t
+
+t.topsis()
+```
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+with open("README.md","r") as fh:
+    long_description=fh.read()
+setup(
+    name='TOPSIS-ShivamPundir-101803158',
+    version='0.0.1',
+    description='TOPSIS Implementation',
+    py_modules=["TOPSIS"],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy>=1.18.1",
+        "pandas>=1.0.5"
+    ],
+    extras_require={
+        "dev":[
+            "pytest>=3.7",
+        ]
+    },
+    url="",
+    author="Shivam Pundir",
+    author_email="shivampundir009@gmail.com"
+)
